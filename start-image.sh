7481762b8f23f0d1de5f4392560c7d689f576248
R0ck <peter.bacsai@gmail.com>
2020-03-29 20:05:16 +0200

    Added pymldb + Corrected startscript

--- start-image.sh
+++ start-image.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 _VERSION=$1
+_DATE=`date '+%Y-%m-%d'`
 if [ -z "$_VERSION" ]
 then
-docker run --gpus all -d -p 8888:8888 -e KERAS_BACKEND=tensorflow -v /notebook:/notebook tf_keras
+docker run --gpus all -d -p 8888:8888 -e KERAS_BACKEND=tensorflow -v /notebook:/notebook tf_keras_$_DATE
 else
 docker run --gpus all -d -p 8888:8888 -e KERAS_BACKEND=tensorflow -v /notebook:/notebook tf_keras_$_VERSION
 fi
